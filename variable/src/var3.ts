319ca3e5e2a6482460d49ce30dc2cfb79908a657
swyoo <getampain@gmail.com>
2020-04-16 18:01:35 +0900

    2020-04-16

--- variable/src/var3.ts
+++ variable/src/var3.ts
@@ -6,4 +6,16 @@ let returnFuction:(userAge : number) => number = function(userAge){
 }
 
 
+enum enumTest1 {first, second, third};
+enum enumTest2 {first = 10, second = 20, third = 30};
+enum enumTest3 {first = "10", second = "20", third = "30"};
+
+
+let enumCopy:enumTest3 = enumTest3.second;
+
+
+
+console.log(enumTest1);
+console.log(enumTest2);
+console.log(enumTest3);
 console.log(returnFuction(23));
