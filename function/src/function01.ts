75508be79cc3f5d0d18ff89dfe3609059f08f75e
swyoo <getampain@naver.com>
2020-04-26 23:23:41 +0900

    2020-04-26

--- function/src/function01.ts
+++ function/src/function01.ts
@@ -30,3 +30,32 @@
     //후반 파라메터만 넣는건 안되는듯하다
     //console.log(pow(,3));
 }
+
+
+function restParam(a: any, b: any, c: any, ...restP: any[]){
+    console.log(a+b+c+restP.join("__"));
+}
+
+restParam("1","2","3",4,5,6,7,8,9,0);
+
+
+//arguments 는 해당 함수가 받은 매게변수를 순서대로 받아오는거다
+function colors(a:string){
+    var rest = [];
+    for(var _i=1;_i<arguments.length;_i++){
+        rest[_i - 1] = arguments[_i];
+    }
+    return a + " " + rest.join(" ");
+}
+
+
+//매개변수에서 ?와 = 기본값 지정은 동시사용이 불가능하다
+function sum2(a:number, b?:number){
+    if(b === undefined){
+        b = 0;
+    }
+
+    console.log(a+b);
+}
+
+sum2(12);
