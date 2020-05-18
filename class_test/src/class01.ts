65d4807aae0db22868bc3b9ad10aaa77ce8a4254
swyoo <getampain@naver.com>
2020-05-18 20:48:41 +0900

    add

--- class_test/src/class01.ts
+++ class_test/src/class01.ts
@@ -103,9 +103,79 @@
 
     get과 set이 없는 프로퍼티는 자동으로 읽기 전용으로 추정됩니다. 이것은 여러분의 코드에서 .d.ts 파일을 생성할 때 유용합니다. 왜냐하면 여러분의 프로퍼티 사용자가 그것을 변경할 수 없다는 것을 알 수 있기 때문입니다
     
+
     클레스에서 function으로 처리가 가능한 부분을 굳이 getter setter를 지정해서 사용하는 이유는 위와 같다고 생각함
     게터 세터가 없기때문에 자동으로 읽기전용으로 추정됨
 
 
     */
+
+
+
+    /*
+
+            *****
+            중요한점은 상위클래스의 변수값, 함수값은
+            자식클래스에서는 .변수, .함수값으로 가져오는것이 불가능하다.
+            즉 public으로 선언을 했다고 하더라도 반드시 geter seter그리고 
+            메소드를 통하여 값을 가져와야 한다.
+
+
+            또한 상위와 하위가 변수명이 같다면 동시에 수정된다
+            
+    */
+    class PC{
+        constructor(public hddCapacity: string){}
+
+        private ram: string = "0G";
+
+        set ramCapacity(val: string){
+            this.ram = val;
+        }
+        get ramCapacity(){
+            return this.ram;
+        }
+
+        protected getHddCapacity(){
+            return this.hddCapacity;
+        }
+    }
+
+    class Desktop extends PC{
+        constructor(public hddCapacity:string){
+            super(hddCapacity);
+        }
+
+        /*
+
+        constructor(public hddCapacity2:string){
+            super(hddCapacity2);
+            이런것도 가능하다
+        }
+
+        */
+
+        getInfo(){
+            console.log(super.getHddCapacity()+"  "+super.hddCapacity);
+            console.log(this.getHddCapacity()+"  "+this.hddCapacity);
+
+            this.hddCapacity = "2000G";
+            console.log(super.getHddCapacity()+"  "+super.hddCapacity);
+            console.log(this.getHddCapacity()+"  "+this.hddCapacity);
+
+
+            //게터 세터를 사용하여 이런식으로 사용가능하다
+            super.ramCapacity = "16G";
+            console.log(super.ramCapacity + "  "+ this.ramCapacity);
+
+            this.ramCapacity = "12G";
+            console.log(super.ramCapacity + "  "+ this.ramCapacity);
+
+
+        }
+    }
+
+    let abcdef = new Desktop("222G");
+    abcdef.getInfo();
+
 }
